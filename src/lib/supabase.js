096f8412b41057ae46cf6c9dd2c3cfdbe2447bf9
Beldion <bgbalanan@gmail.com>
2026-03-18 20:51:00 +0800

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,7 +3,9 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+console.log('URL:', supabaseUrl)
+console.log('KEY:', supabaseAnonKey)
+
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-console.log('URL:', import.meta.env.VITE_SUPABASE_URL)
-console.log('KEY:', import.meta.env.VITE_SUPABASE_ANON_KEY)
